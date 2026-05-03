339017d79215f9f5b376c137d95052a37799ca42
Mahad Siddiqui <mahadsiddiqui07@outlook.com>
2026-05-03 10:45:21 +0100

    cleanup

---
+++ main/pins.h
@@ -0,0 +1,17 @@
+#pragma once
+
+// OLED (7-PIN SPI SH1106)
+#define CLK   18
+#define MOSI  17
+#define CS    16
+#define DC    15
+#define RES   14
+
+// KEYPAD (3X4 MATRIX MEMBRANE)
+#define P0 13
+#define P1 12
+#define P2 11
+#define P3 10
+#define P4 9
+#define P5 8
+#define P6 7
